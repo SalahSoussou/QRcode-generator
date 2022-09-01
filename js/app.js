cd3058083f7f350cdf3e1f2f921ac85ffcf981ac
SalahSoussou <kaka79702@gmail.com>
2022-09-01 12:58:08 +0100

    ok

--- js/app.js
+++ js/app.js
@@ -9,13 +9,30 @@ const hideLoder = () => {
 };
 hideLoder();
 
+const qrCodeGen = (url, size, dColor, lColor) => {
+  const qrcode = new QRCode(document.getElementById("qrcode"), {
+    text: url,
+    width: size,
+    height: size,
+    colorDark: dColor,
+    colorLight: lColor,
+    //   correctLevel: QRCode.CorrectLevel.H,
+  });
+};
+
 form.onsubmit = (e) => {
   e.preventDefault();
   const url = document.getElementById("srcText").value,
+    dColor = document.getElementById("dColor").value,
+    lColor = document.getElementById("lColor").value,
     size = document.getElementById("size").value;
   if (url === "") {
     alert("Please enter a URL");
   } else {
     showLoder();
+    setTimeout(() => {
+      hideLoder();
+      qrCodeGen(url, size, dColor, lColor);
+    }, 1500);
   }
 };
